348daabd0d2e720f1c73f2ae56eb1e7e350d1a15
yuki_yano <yuki.ycino@gmail.com>
2020-05-18 20:03:50 +0900

    Add environment variable for dev mode

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,9 @@
 import { NvimPlugin } from "neovim"
 
 module.exports = (plugin: NvimPlugin) => {
-  plugin.setOptions({ dev: true, alwaysInit: true })
+  if (process.env.REMOTE_PLUGIN_TS_DEBUG === "1") {
+    plugin.setOptions({ dev: true, alwaysInit: true })
+  }
 
   plugin.registerCommand("HelloTypeScript", () => {
     plugin.nvim.command("echo 'Hello TypeScript!'")
