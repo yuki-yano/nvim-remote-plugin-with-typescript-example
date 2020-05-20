f925418a9e75366c9e55513f675a3c661fe2f1ff
yuki_yano <yuki.ycino@gmail.com>
2020-05-20 19:41:29 +0900

    Add Jest

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { NvimPlugin } from "neovim"
 
+import { sum } from "./sum"
+
 module.exports = (plugin: NvimPlugin) => {
   if (process.env.REMOTE_PLUGIN_TS_DEBUG === "1") {
     plugin.setOptions({ dev: true, alwaysInit: true })
@@ -9,4 +11,6 @@ module.exports = (plugin: NvimPlugin) => {
     plugin.nvim.command("echo 'Hello TypeScript!'")
     console.log("Hello TypeScript!")
   })
+
+  plugin.registerFunction("TypeScriptSum", sum, { sync: true })
 }
---
+++ src/sum.ts
@@ -0,0 +1,7 @@
+export const sum = (numbers: Array<number>) => {
+  const num1 = numbers[0]
+  const num2 = numbers[1]
+
+  const sum = num1 + num2
+  return sum
+}
